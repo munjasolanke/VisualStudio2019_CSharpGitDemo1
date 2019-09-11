3b7f96b25b65ab32fc55c5aedbd0529116edde62
munjasolanke <54603212+munjasolanke@users.noreply.github.com>
2019-09-10 20:02:47 -0400

    commit
    commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VisualStudio2019_CSharpGitDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("how many years of professional experience do you have");
+            string input = Console.ReadLine();
+
+            switch(int.Parse(input))
+            {
+                case 0:
+                    Console.WriteLine("need lots of practrice");
+                    break;
+
+                case 1:
+                    Console.WriteLine("looks like you need a little more");
+                    break;
+
+                case 2:
+                    Console.WriteLine("you are doing good");
+                    break;
+
+                default:
+                    Console.WriteLine("excellent");
+                    break;
+            }
+
+        }
+    }
+}
